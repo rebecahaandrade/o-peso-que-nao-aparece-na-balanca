72edfadf8551dcb1a9fa6c230c114e33c2ea92cc
Albert Thomaz <thzalbert@gmail.com>
2026-02-14 23:38:12 -0300

    chore: change repo name

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/peso-sem-peso">
+      <BrowserRouter basename="/o-peso-que-nao-aparece-na-balanca">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/peso-sem-peso/",
+  base: "/o-peso-que-nao-aparece-na-balanca/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
